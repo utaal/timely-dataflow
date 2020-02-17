4c3f297107bbcd2b74402ebb59d66721e9b0871a
decovas <sara.decova@gmail.com>
2020-02-17 15:32:09 +0100

    Added examples for benchmarking the new logger on the cluster.

---
+++ timely/examples/barrier2.rs
@@ -0,0 +1,83 @@
+extern crate timely;
+extern crate hdrhist;
+extern crate getopts;
+
+use std::time::Instant;
+use timely::dataflow::channels::pact::Pipeline;
+use timely::dataflow::operators::{Feedback, ConnectLoop};
+use timely::dataflow::operators::generic::operator::Operator;
+
+use timely::logging::TimelyEvent;
+use timely::progress::reachability::TrackerEvent;
+
+fn main() {
+    let iterations = std::env::args().nth(1).unwrap().parse::<usize>().unwrap_or(1_000_000);
+    // currently need timely's full option set to parse args
+    let mut opts = getopts::Options::new();
+    opts.optopt("w", "workers", "", "");
+    opts.optopt("n", "processes", "", "");
+    opts.optopt("p", "process_id", "", "");
+    opts.optopt("h", "hostfile", "", "");
+    opts.optflag("s", "serialize", "use the zero_copy serialising allocator");
+    if let Ok(matches) = opts.parse(std::env::args().skip(2)) {
+        let workers: usize = matches.opt_str("w").map(|x| x.parse().unwrap_or(1)).unwrap_or(1);
+        let serialize: bool = matches.opt_present("s");
+        macro_rules! worker_closure { () => (move |worker| {
+            worker.log_register().insert::<TimelyEvent,_>("timely", |_, data|
+                data.iter().for_each(|x| println!("{}", serde_json::to_string(&x.2).unwrap()))
+            );
+
+            worker.log_register().insert::<TrackerEvent,_>("timely/tracker", |_, data|
+                data.iter().for_each(|x| println!("{}", serde_json::to_string(&x.2).unwrap()))
+            );
+
+            let index = worker.index();
+
+            worker.dataflow(move |scope| {
+                let (handle, stream) = scope.feedback::<usize>(1);
+                let mut hist = hdrhist::HDRHist::new();
+                let mut t0 = Instant::now();
+                stream.unary_notify(
+                    Pipeline,
+                    "Barrier",
+                    vec![0],
+                    move |_, _, notificator| {
+                        while let Some((cap, _count)) = notificator.next() {
+                            let t1 = Instant::now();
+                            let duration = t1.duration_since(t0);
+                            hist.add_value(duration.as_secs() * 1_000_000_000u64 + duration.subsec_nanos() as u64);
+                            t0 = t1;
+                            let time = *cap.time() + 1;
+                            if time < iterations {
+                                notificator.notify_at(cap.delayed(&time));
+                            } else {
+                                if index == 0 {
+                                    println!("-------------\nSummary:\n{}", hist.summary_string());
+                                    println!("-------------\nCDF:");
+                                    for entry in hist.ccdf_upper_bound() {
+                                        println!("{:?}", entry);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                )
+                    .connect_loop(handle);
+            });
+        });
+        }
+        if !serialize {
+            timely::execute::execute_from_args(std::env::args().skip(2), worker_closure!()).unwrap();
+        } else {
+            eprintln!("NOTE: thread allocators zerocopy, this ignores -p and -n");
+            let allocators =
+                ::timely::communication::allocator::zero_copy::allocator_process::ProcessBuilder::new_vector(workers);
+            timely::execute::execute_from(
+                allocators, Box::new(()), worker_closure!()).unwrap();
+        }
+        // std::env::args().skip(2).filter(|arg| arg != "-s"), 
+    } else {
+        println!("error parsing arguments");
+        println!("usage:\tbarrier <iterations> (worker|process) [timely options]");
+    }
+}
---
+++ timely/examples/open_loop2.rs
@@ -0,0 +1,140 @@
+extern crate timely;
+extern crate hdrhist;
+
+use timely::dataflow::{InputHandle, ProbeHandle};
+use timely::dataflow::operators::{Input, Probe, Exchange};
+
+fn main() {
+    // currently need timely's full option set to parse args
+    let mut args = std::env::args();
+    args.next();
+
+    let rate: usize = args.next().expect("Must specify rate").parse().expect("Rate must be an usize");
+    let duration_s: usize = args.next().expect("Must specify duration_s").parse().expect("duration_s must be an usize");
+
+
+    let mut opts = getopts::Options::new();
+    opts.optopt("w", "workers", "", "");
+    opts.optopt("n", "processes", "", "");
+    opts.optopt("p", "process_id", "", "");
+    opts.optopt("h", "hostfile", "", "");
+    opts.optflag("s", "serialize", "use the zero_copy serialising allocator");
+
+    if let Ok(matches) = opts.parse(std::env::args().skip(3)) {
+
+        let workers: usize = matches.opt_str("w").map(|x| x.parse().unwrap_or(1)).unwrap_or(1);
+        let serialize: bool = matches.opt_present("s");
+        macro_rules! worker_closure { () => (move |worker| {
+
+            let index = worker.index();
+
+            // re-synchronize all workers (account for start-up).
+            timely::synchronization::Barrier::new(worker).wait();
+
+            let timer = std::time::Instant::now();
+
+            let mut input = InputHandle::new();
+            let mut probe = ProbeHandle::new();
+
+            // Create a dataflow that discards input data (just syncronizes).
+            worker.dataflow(|scope| {
+                scope
+                    .input_from(&mut input)     // read input.
+                    .exchange(move |x| *x as u64)
+                    .probe_with(&mut probe);    // observe output.
+            });
+
+            let ns_per_request = 1_000_000_000 / rate;
+            let mut insert_counter = index;           // counts up as we insert records.
+            let mut retire_counter = index;           // counts up as we retire records.
+
+            let mut inserted_ns = 0;
+
+            // We repeatedly consult the elapsed time, and introduce any data now considered available.
+            // At the same time, we observe the output and record which inputs are considered retired.
+
+            let counter_limit = rate * duration_s;
+
+            let mut hist = hdrhist::HDRHist::new();
+
+            if index == 0 {
+                while retire_counter < counter_limit {
+
+                    // Open-loop latency-throughput test, parameterized by offered rate `ns_per_request`.
+                    let elapsed = timer.elapsed();
+                    let elapsed_ns = elapsed.as_secs() * 1_000_000_000 + (elapsed.subsec_nanos() as u64);
+
+                    // Determine completed ns.
+                    let acknowledged_ns: u64 = probe.with_frontier(|frontier| frontier[0]);
+
+                    // Notice any newly-retired records.
+                    while ((retire_counter * ns_per_request) as u64) < acknowledged_ns && retire_counter < counter_limit {
+                        let requested_at = (retire_counter * ns_per_request) as u64;
+                        let latency_ns = elapsed_ns - requested_at;
+
+                        hist.add_value(latency_ns);
+
+                        retire_counter += 1;
+                    }
+
+                    // Now, should we introduce more records before stepping the worker?
+                    // Three choices here:
+                    //
+                    //   1. Wait until previous batch acknowledged.
+                    //   2. Tick at most once every millisecond-ish.
+                    //   3. Geometrically increase outstanding batches.
+
+                    // Technique 1:
+                    // let target_ns = if acknowledged_ns >= inserted_ns { elapsed_ns } else { inserted_ns };
+
+                    // Technique 2:
+                    // let target_ns = elapsed_ns & !((1 << 20) - 1);
+
+                    // Technique 3:
+                    let target_ns = {
+                        let delta: u64 = inserted_ns - acknowledged_ns;
+                        let bits = ::std::mem::size_of::<u64>() * 8 - delta.leading_zeros() as usize;
+                        let scale = ::std::cmp::max((1 << bits) / 4, 1024);
+                        elapsed_ns & !(scale - 1)
+                    };
+
+                    // Common for each technique.
+                    if inserted_ns < target_ns {
+
+                        while ((insert_counter * ns_per_request) as u64) < target_ns {
+                            input.send(insert_counter);
+                            insert_counter += 1;
+                        }
+                        input.advance_to(target_ns);
+                        inserted_ns = target_ns;
+                    }
+
+                    worker.step();
+                }
+            } else {
+                input.close();
+            }
+
+            // Report observed latency measurements.
+            if index == 0 {
+                println!("-------------\nSummary:\n{}", hist.summary_string());
+                println!("-------------\nCDF:");
+                for entry in hist.ccdf_upper_bound() {
+                    println!("{:?}", entry);
+                }
+            }
+        });
+        }
+        if !serialize {
+            ::timely::execute::execute_from_args(std::env::args().skip(3), worker_closure!()).unwrap();
+        } else {
+            eprintln!("NOTE: thread allocators zerocopy, this ignores -p and -n");
+            let allocators =
+                ::timely::communication::allocator::zero_copy::allocator_process::ProcessBuilder::new_vector(workers);
+            ::timely::execute::execute_from(allocators, Box::new(()), worker_closure!()).unwrap();
+        }
+    } else {
+        println!("error parsing arguments");
+        println!("usage:\topenloop <rate> <duration> (worker|process) [timely options]");
+    }
+}
